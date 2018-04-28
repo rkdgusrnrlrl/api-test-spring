1af171d676a4e70802aa06c3115d0851e2be091a
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-04-28 23:21:29 +0900

    fix:delete fongo source and import fongo by gradle

--- src/main/java/com/github/fakemongo/impl/Filter.java
+++
@@ -1,7 +0,0 @@
-package com.github.fakemongo.impl;
-
-import com.mongodb.DBObject;
-
-public interface Filter {
-  boolean apply(DBObject o);
-}
--- src/main/java/com/github/fakemongo/impl/ValueFilter.java
+++
@@ -1,9 +0,0 @@
-package com.github.fakemongo.impl;
-
-/**
- * Filter for any value, not just DBObject as {@link Filter}
- * Needed by $pull command that can filter out both sub-documents and primitive values like string, etc
- */
-public interface ValueFilter {
-    boolean apply(Object object);
-}

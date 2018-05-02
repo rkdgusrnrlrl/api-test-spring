de40580782da1c354fe57e4a4086bf8b0342d0ff
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-05-02 21:53:45 +0900

    refec:change test mock

--- src/main/java/me/dakbutfly/apitest/controller/ApiCallerController.java
+++ src/main/java/me/dakbutfly/apitest/controller/ApiCallerController.java
@@ -1,8 +1,27 @@
 package me.dakbutfly.apitest.controller;
 
+import com.google.common.collect.ImmutableMap;
+import me.dakbutfly.apitest.entity.ApiCaller;
+import me.dakbutfly.apitest.repository.ApiCallerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class ApiCallerController {
+
+    @Autowired
+    private ApiCallerRepository apiCallerRepository;
+
+    @RequestMapping(value = "/api/apicallers",method = RequestMethod.GET)
+    public List<ApiCaller> list() {
+        return Collections.<ApiCaller>emptyList();
+    }
 
 }
--- src/test/java/me/dakbutfly/apitest/controller/TestApiCallerController.java
+++ src/test/java/me/dakbutfly/apitest/controller/TestApiCallerController.java
@@ -1,8 +1,14 @@
 package me.dakbutfly.apitest.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.dakbutfly.apitest.demo.DemoApplication;
+import me.dakbutfly.apitest.entity.ApiCaller;
+import me.dakbutfly.apitest.repository.ApiCallerRepository;
 import org.junit.*;
 import org.junit.runner.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.*;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,26 +16,37 @@ import org.springframework.boot.test.mock.mockito.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.assertj.core.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(ApiCallerController.class)
-@ContextConfiguration(classes = {DemoApplication.class})
 public class TestApiCallerController {
 
-    @Autowired
+
     private MockMvc mockMvc;
+
+    @Mock
+    private ApiCallerRepository apiCallerRepository;
+
+    @InjectMocks
+    private ApiCallerController apiCallerController;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(apiCallerController).build();
+    }
 
     @Test
     public void testGetApiCallerList() throws Exception {
         mockMvc.perform(get("/api/apicallers"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("apiCallerList.length()").value(0));
+                .andExpect(jsonPath("$.length()").value(0));
     }
 }
